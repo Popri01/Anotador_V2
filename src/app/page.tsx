9db925305806829b19a9c530fbab11d660d28d54
Popri01 <lucas.popri@gmail.com>
2024-10-21 18:14:41 -0300

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,11 +84,11 @@ export default function HomePage() {
   }
 
   return (
-    <div className="flex h-[670px] w-full items-center justify-center gap-4 bg-[url('/Fondo.jpg')] bg-cover bg-center font-nanum text-[#26619C] lg:h-[850px] lg:w-[800px] lg:gap-20">
+    <div className="flex h-[670px] w-full items-center justify-center  bg-[url('/Fondo.jpg')] bg-cover bg-center font-nanum text-[#26619C] lg:h-[850px] lg:w-[800px] lg:gap-8">
       <div className="flex flex-col items-center md:w-auto">
         <h1 className="text-2xl md:text-3xl">Nosotros</h1>
 
-        <div className="flex h-[400px] flex-col items-center md:h-[500px]">
+        <div className="flex h-[400px] w-[150px] flex-col items-center md:h-[500px] md:w-[200px]">
           {renderCuadrado(puntosNosotros)}
         </div>
 
@@ -118,7 +118,9 @@ export default function HomePage() {
       <div className="flex flex-col items-center md:w-auto">
         <h1 className="text-2xl md:text-3xl">Ellos</h1>
 
-        <div className="flex h-[400px] flex-col md:h-[500px]">{renderCuadrado(puntosEllos)}</div>
+        <div className="flex h-[400px] w-[150px] flex-col items-center md:h-[500px] md:w-[200px]">
+          {renderCuadrado(puntosEllos)}
+        </div>
 
         <div className="mt-4 flex items-center space-x-4 lg:mt-8">
           <button
